7d1bba4ac7b0189e63bd74389110a71a21eda259
divyanshusharma3031 <divyanshukatyan_co20b3_55@dtu.ac.in>
2022-05-28 15:11:27 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import "../src/Header.css";
+import Searchbox from "./Searchbox";
+import Questions from "./Questions";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Searchbox /> */}
+      <Questions/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
